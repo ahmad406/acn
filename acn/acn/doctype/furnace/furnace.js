d818ff600924eb3e012eb3acbba5784a8fee4508
cloudtrac <frappe@vmi2514774.contaboserver.net>
2025-03-25 20:06:44 +0530

    masters

---
+++ acn/acn/doctype/furnace/furnace.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ahmad Sayyed and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Furnace", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ acn/acn/doctype/job_plan_scheduler/test_job_plan_scheduler.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ahmad Sayyed and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJobPlanScheduler(FrappeTestCase):
+	pass
