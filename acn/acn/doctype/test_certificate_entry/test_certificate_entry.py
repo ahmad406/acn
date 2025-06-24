38d5edcdcdfb7f5996010548c00a3d690db806aa
ahmad406 <ahmadsayyed66@gmailcom>
2025-06-24 19:20:38 +0530

    24-6-25

--- acn/acn/doctype/test_certificate_entry/test_certificate_entry.py
+++ acn/acn/doctype/test_certificate_entry/test_certificate_entry.py
@@ -62,87 +62,89 @@ class TestCertificateentry(Document):
 
 				break
 		for k in li.parameters:
-			row=self.append("test_parameters_details",{})
-			row.test_parameters=k.control_parameter
-			row.qty_checked=self.accepted_qty_in_nos
+			if self.job_card_id==k.job_card_id:
+				row=self.append("test_parameters_details",{})
+				row.test_parameters=k.control_parameter
+				row.qty_checked=self.accepted_qty_in_nos
+				frappe.errprint("in")
 
-			row.reference_standard=self.get_standard(k.control_parameter)
-			row.print_sequence_no = frappe.db.get_value('Internal Control Parameter', k.control_parameter, 'tc_order_no')
-
-
-			row.acceptance_criteria_from=k.result_value_from
-			row.acceptance_criteria_to=k.result_value_to
-			row.scale=k.scale
-			row.other_detail_1=k.other_detail_1
-			row.other_detail_2=k.other_detail_2
-			row.other_detail_3=k.other_detail_3
-			row.other_detail_4=k.other_detail_4
-			row.other_detail_5=k.other_detail_5
-			row.other_detail_6=k.other_detail_6
-			row.other_detail_7=k.other_detail_7
-			row.other_detail_8=k.other_detail_8
-			row.other_detail_9=k.other_detail_9
-			row.ref_standard=k.ref_standard
+				row.reference_standard=self.get_standard(k.control_parameter)
+				row.print_sequence_no = frappe.db.get_value('Internal Control Parameter', k.control_parameter, 'tc_order_no')
 
 
-			row.core=k.core
-			row.case=k.case
+				row.acceptance_criteria_from=k.result_value_from
+				row.acceptance_criteria_to=k.result_value_to
+				row.scale=k.scale
+				self.other_detail_1=k.other_detail_1
+				self.other_detail_2=k.other_detail_2
+				self.other_detail_3=k.other_detail_3
+				self.other_detail_4=k.other_detail_4
+				self.other_detail_5=k.other_detail_5
+				self.other_detail_6=k.other_detail_6
+				self.other_detail_7=k.other_detail_7
+				self.other_detail_8=k.other_detail_8
+				self.other_detail_9=k.other_detail_9
+				self.ref_standard=k.ref_standard
 
 
-			row.case_depth_1_1=k.case_depth_1_1
-			row.case_depth_1_2=k.case_depth_1_2
-			row.case_depth_1_3=k.case_depth_1_3
-			row.case_depth_1_4=k.case_depth_1_4
-			row.case_depth_1_5=k.case_depth_1_5
-			row.case_depth_1_6=k.case_depth_1_6
-			row.case_depth_1_6=k.case_depth_1_6
-			row.case_depth_1_7=k.case_depth_1_7
-			row.case_depth_1_8=k.case_depth_1_8
-			row.case_depth_1_9=k.case_depth_1_9
-			row.case_depth_1_10=k.case_depth_1_10
-
-			row.hardness_1_1=k.hardness_1_1
-			row.hardness_1_2=k.hardness_1_2
-			row.hardness_1_3=k.hardness_1_3
-			row.hardness_1_4=k.hardness_1_4
-			row.hardness_1_5=k.hardness_1_5
-			row.hardness_1_6=k.hardness_1_6
-			row.hardness_1_7=k.hardness_1_7
-			row.hardness_1_8=k.hardness_1_8
-			row.hardness_1_9=k.hardness_1_9
-			row.hardness_1_10=k.hardness_1_10
+				self.core=k.core
+				self.case=k.case
 
 
-			row.case_depth_2_1=k.case_depth_2_1
-			row.case_depth_2_2=k.case_depth_2_2
-			row.case_depth_2_3=k.case_depth_2_3
-			row.case_depth_2_4=k.case_depth_2_4
-			row.case_depth_2_5=k.case_depth_2_5
-			row.case_depth_2_6=k.case_depth_2_6
-			row.case_depth_2_7=k.case_depth_2_7
-			row.case_depth_2_8=k.case_depth_2_8
-			row.case_depth_2_9=k.case_depth_2_9
-			row.case_depth_2_10=k.case_depth_2_10
+				self.case_depth_1_1=k.case_depth_1_1
+				self.case_depth_1_2=k.case_depth_1_2
+				self.case_depth_1_3=k.case_depth_1_3
+				self.case_depth_1_4=k.case_depth_1_4
+				self.case_depth_1_5=k.case_depth_1_5
+				self.case_depth_1_6=k.case_depth_1_6
+				self.case_depth_1_6=k.case_depth_1_6
+				self.case_depth_1_7=k.case_depth_1_7
+				self.case_depth_1_8=k.case_depth_1_8
+				self.case_depth_1_9=k.case_depth_1_9
+				self.case_depth_1_10=k.case_depth_1_10
+
+				self.hardness_1_1=k.hardness_1_1
+				self.hardness_1_2=k.hardness_1_2
+				self.hardness_1_3=k.hardness_1_3
+				self.hardness_1_4=k.hardness_1_4
+				self.hardness_1_5=k.hardness_1_5
+				self.hardness_1_6=k.hardness_1_6
+				self.hardness_1_7=k.hardness_1_7
+				self.hardness_1_8=k.hardness_1_8
+				self.hardness_1_9=k.hardness_1_9
+				self.hardness_1_10=k.hardness_1_10
 
 
-			row.hardness_2_1=k.hardness_2_1
-			row.hardness_2_2=k.hardness_2_2
-			row.hardness_2_3=k.hardness_2_3
-			row.hardness_2_4=k.hardness_2_4
-			row.hardness_2_5=k.hardness_2_5
-			row.hardness_2_6=k.hardness_2_6
-			row.hardness_2_7=k.hardness_2_7
-			row.hardness_2_8=k.hardness_2_8
-			row.hardness_2_9=k.hardness_2_9
-			row.hardness_2_10=k.hardness_2_10
+				self.case_depth_2_1=k.case_depth_2_1
+				self.case_depth_2_2=k.case_depth_2_2
+				self.case_depth_2_3=k.case_depth_2_3
+				self.case_depth_2_4=k.case_depth_2_4
+				self.case_depth_2_5=k.case_depth_2_5
+				self.case_depth_2_6=k.case_depth_2_6
+				self.case_depth_2_7=k.case_depth_2_7
+				self.case_depth_2_8=k.case_depth_2_8
+				self.case_depth_2_9=k.case_depth_2_9
+				self.case_depth_2_10=k.case_depth_2_10
 
 
-			row.lab_entry_no=k.lab_entry_no
+				self.hardness_2_1=k.hardness_2_1
+				self.hardness_2_2=k.hardness_2_2
+				self.hardness_2_3=k.hardness_2_3
+				self.hardness_2_4=k.hardness_2_4
+				self.hardness_2_5=k.hardness_2_5
+				self.hardness_2_6=k.hardness_2_6
+				self.hardness_2_7=k.hardness_2_7
+				self.hardness_2_8=k.hardness_2_8
+				self.hardness_2_9=k.hardness_2_9
+				self.hardness_2_10=k.hardness_2_10
+
+
+				row.lab_entry_no=k.lab_entry_no
 		jb=frappe.get_doc("Job Card for process",self.job_card_id)
 		for f in jb.parameters_with_acceptance_criteria:
 			rw=self.append("parameters",{})
 			rw.internal_process=f.internal_process
-			rw.control_parameter=f.control_parameter
+			rw.process_parameter=f.control_parameter
 			rw.minimum_value=f.minimum_value
 			rw.maximum_value=f.maximum_value
 			rw.scale=f.scale
