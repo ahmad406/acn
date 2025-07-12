41db20259e0f5926b5b49a297540097a4ccf19af
Ahmad <ahmadsayyed66@.com>
2025-07-12 23:21:00 +0530

    suntottal

--- acn/acn/report/sub_account_wise_expense_list/sub_account_wise_expense_list.py
+++ acn/acn/report/sub_account_wise_expense_list/sub_account_wise_expense_list.py
@@ -1,24 +1,21 @@
-# Copyright (c) 2025, Ahmad Sayyed and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.utils import flt
 
 def execute(filters=None):
     columns = get_columns()
     data = get_data(filters)
-    return columns, data
+    processed_data = process_data(data)
+    return columns, processed_data
 
 def get_columns():
     return [
-        {"label": "Account", "fieldname": "account", "fieldtype": "Link", "options": "Account", "width": 180},
-        {"label": "Sub Account", "fieldname": "sub_account", "fieldtype": "Data", "width": 200},
-        {"label": "Total Debit Amount", "fieldname": "debit_amount", "fieldtype": "Currency", "width": 150},
+        {"label": "Account", "fieldname": "account", "fieldtype": "Link", "options": "Account", "width": 320},
+        {"label": "Sub Account", "fieldname": "sub_account", "fieldtype": "Data", "width": 260},
+        {"label": "Total Debit Amount", "fieldname": "debit_amount", "fieldtype": "Currency", "width": 220},
     ]
 
 def get_data(filters):
     conditions = ["IFNULL(sub_account, '') != ''"]
-
     if filters.get("company"):
         conditions.append("company = %(company)s")
     if filters.get("from_date"):
@@ -31,7 +28,6 @@ def get_data(filters):
         conditions.append("sub_account = %(sub_account)s")
 
     where_clause = " AND ".join(conditions)
-
     query = f"""
         SELECT
             company,
@@ -47,5 +43,44 @@ def get_data(filters):
         ORDER BY
             company, account, sub_account
     """
-
     return frappe.db.sql(query, filters, as_dict=True)
+
+def process_data(data):
+    if not data:
+        return []
+    
+    processed_data = []
+    current_account = None
+    account_total = 0
+    grand_total = 0
+    
+    for row in data:
+        if current_account != row.account:
+            if current_account is not None:
+                processed_data.append({
+                     "account":current_account,
+                    "sub_account": "<strong>Sub Total</strong>",
+                    "debit_amount": account_total,
+                })
+            
+            current_account = row.account
+            account_total = 0
+        
+        processed_data.append(row)
+        account_total += flt(row.debit_amount)
+        grand_total += flt(row.debit_amount)
+    
+    if current_account is not None:
+        processed_data.append({
+            "account":current_account,
+            "sub_account": "<strong>Total</strong>",
+            "debit_amount": account_total,
+        })
+    
+    processed_data.append({
+        "account": "",
+        "sub_account": "<strong>Grand Total</strong>",
+        "debit_amount": grand_total,
+    })
+    
+    return processed_data
