e9d2616b45107d0f5e4d4dd888226ada10bb47ae
Ahmad <ahmadsayyed66@.com>
2026-02-23 19:39:25 +0530

    2302-26

--- acn/acn/doctype/job_card_for_process/job_card_for_process.py
+++ acn/acn/doctype/job_card_for_process/job_card_for_process.py
@@ -37,60 +37,134 @@ def get_item_tax_rate(doc, account):
 
     return "@ {0}%".format(int(rate))
 
+
+import frappe
+from frappe.utils import flt
+
 @frappe.whitelist()
 def get_dispatch_details(doc):
-	sql="""SELECT
-		CASE WHEN rn = 1 THEN job_card ELSE "" END AS job_card,
-		CASE WHEN rn = 1 THEN test_certificate ELSE "" END AS test_certificate,
-		delivery_note,
-		CASE WHEN rn = 1 THEN sales_invoice ELSE "" END AS sales_invoice,
-		CASE WHEN rn = 1  THEN DATE_FORMAT(invoice_date, '%d-%m-%Y') ELSE "" END AS invoice_date,
-        	 CASE WHEN rn = 1 THEN kgs ELSE NULL END AS kgs,
-    CASE WHEN rn = 1 THEN nos ELSE NULL END AS nos
-	FROM (
-		SELECT
-			jc.name        AS job_card,
-			tc.name        AS test_certificate,
-			dli.parent     AS delivery_note,
-			si.name        AS sales_invoice,
-			si.posting_date AS invoice_date,
-                   round(tc.accepted_qty_in_nos,2) AS nos,
-        round(tc.accepted_qty_in_kgs,2) AS kgs,
 
-			ROW_NUMBER() OVER (
-				PARTITION BY dli.parent
-				ORDER BY si.posting_date DESC
-			) AS si_rank,
+    if isinstance(doc, str):
+        jc = frappe.get_doc("Job Card for process", doc)
+    else:
+        jc = doc
 
-			ROW_NUMBER() OVER (
-				PARTITION BY jc.name
-				ORDER BY dli.parent
-			) AS rn
+    tcs = frappe.get_all(
+        "Test Certificate entry",
+        filters={
+            "job_card_id": jc.name,
+            "docstatus": 1
+        },
+        fields=["name"],
+        order_by="name"
+    )
 
-		FROM `tabJob Card for process` jc
+    dns = frappe.db.sql("""
+        SELECT DISTINCT parent
+        FROM `tabDelivery Note Item`
+        WHERE customer_dc_id = %s
+        AND part_no = %s
+        AND docstatus = 1
+        ORDER BY parent
+    """, (jc.customer_dc, jc.part_no), as_dict=True)
 
-		LEFT JOIN `tabTest Certificate entry` tc
-			ON tc.job_card_id = jc.name
-			AND tc.docstatus = 1
+    sis = frappe.db.sql("""
+        SELECT 
+            si.name,
+            si.posting_date,
+            sie.d_qty_in_nos,
+            sie.d_qty_in_kgs
+        FROM `tabSales Invoice Item` sie
+        JOIN `tabSales Invoice` si ON si.name = sie.parent
+        WHERE sie.customer_dc_id = %s
+        AND sie.part_no = %s
+        AND si.docstatus = 1
+        ORDER BY si.posting_date
+    """, (jc.customer_dc, jc.part_no), as_dict=True)
 
-		LEFT JOIN `tabDelivery Note Item` dli
-			ON dli.customer_dc_id = jc.customer_dc
-			AND dli.part_no = jc.part_no
-			AND dli.docstatus = 1
+    max_len = max(len(tcs), len(dns), len(sis))
 
-		LEFT JOIN `tabSales Invoice Item` sie
-			ON sie.customer_dc_id = jc.customer_dc
-			AND sie.part_no = jc.part_no
-			AND sie.docstatus = 1
+    result = []
 
-		LEFT JOIN `tabSales Invoice` si
-			ON si.name = sie.parent
-			AND si.docstatus = 1
+    for i in range(max_len):
+        row = {}
 
-		WHERE jc.name = "{0}"
-	) x
-	WHERE si_rank = 1          
-	ORDER BY delivery_note;
-	""".format(doc.name)
+        row["test_certificate"] = tcs[i]["name"] if i < len(tcs) else ""
+
+        row["delivery_note"] = dns[i]["parent"] if i < len(dns) else ""
+
+        if i < len(sis):
+            row["sales_invoice"] = sis[i]["name"]
+            row["invoice_date"] = (
+                sis[i]["posting_date"].strftime("%d-%m-%Y")
+                if sis[i]["posting_date"] else ""
+            )
+            row["nos"] = flt(sis[i]["d_qty_in_nos"], 2)
+            row["kgs"] = flt(sis[i]["d_qty_in_kgs"], 2)
+        else:
+            row["sales_invoice"] = ""
+            row["invoice_date"] = ""
+            row["nos"] = ""
+            row["kgs"] = ""
+
+        result.append(row)
+
+    return result
+
+# @frappe.whitelist()
+# def get_dispatch_details(doc):
+# 	sql="""SELECT
+# 		CASE WHEN rn = 1 THEN job_card ELSE "" END AS job_card,
+# 		CASE WHEN rn = 1 THEN test_certificate ELSE "" END AS test_certificate,
+# 		delivery_note,
+# 		CASE WHEN rn = 1 THEN sales_invoice ELSE "" END AS sales_invoice,
+# 		CASE WHEN rn = 1  THEN DATE_FORMAT(invoice_date, '%d-%m-%Y') ELSE "" END AS invoice_date,
+#         	 CASE WHEN rn = 1 THEN kgs ELSE NULL END AS kgs,
+#     CASE WHEN rn = 1 THEN nos ELSE NULL END AS nos
+# 	FROM (
+# 		SELECT
+# 			jc.name        AS job_card,
+# 			tc.name        AS test_certificate,
+# 			dli.parent     AS delivery_note,
+# 			si.name        AS sales_invoice,
+# 			si.posting_date AS invoice_date,
+#                    round(tc.accepted_qty_in_nos,2) AS nos,
+#         round(tc.accepted_qty_in_kgs,2) AS kgs,
+
+# 			ROW_NUMBER() OVER (
+# 				PARTITION BY dli.parent
+# 				ORDER BY si.posting_date DESC
+# 			) AS si_rank,
+
+# 			ROW_NUMBER() OVER (
+# 				PARTITION BY jc.name
+# 				ORDER BY dli.parent
+# 			) AS rn
+
+# 		FROM `tabJob Card for process` jc
+
+# 		LEFT JOIN `tabTest Certificate entry` tc
+# 			ON tc.job_card_id = jc.name
+# 			AND tc.docstatus = 1
+
+# 		LEFT JOIN `tabDelivery Note Item` dli
+# 			ON dli.customer_dc_id = jc.customer_dc
+# 			AND dli.part_no = jc.part_no
+# 			AND dli.docstatus = 1
+
+# 		LEFT JOIN `tabSales Invoice Item` sie
+# 			ON sie.customer_dc_id = jc.customer_dc
+# 			AND sie.part_no = jc.part_no
+# 			AND sie.docstatus = 1
+
+# 		LEFT JOIN `tabSales Invoice` si
+# 			ON si.name = sie.parent
+# 			AND si.docstatus = 1
+
+# 		WHERE jc.name = "{0}"
+# 	) x
+# 	WHERE si_rank = 1          
+# 	ORDER BY delivery_note;
+# 	""".format(doc.name)
 		
-	return frappe.db.sql(sql,as_dict=1)
+# 	return frappe.db.sql(sql,as_dict=1)
